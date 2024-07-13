28ea19d2a5b1387c1649e41ad464d6dca3871f80
Morteza <mortezaa.fathi@gmail.com>
2024-07-13 15:22:47 +0330

    Fix style

--- app.js
+++ app.js
@@ -1,68 +1,48 @@
-// JavaScript to handle opening and closing of mobile menu
-const menuToggle = document.getElementById("menu-toggle");
-const closeMenuBtn = document.getElementById("close-menu");
-const mobileMenu = document.getElementById("mobile-menu");
-
-menuToggle.addEventListener("click", function () {
-  mobileMenu.classList.toggle("hidden");
-});
-
-closeMenuBtn.addEventListener("click", function () {
-  mobileMenu.classList.add("hidden");
-});
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-
-gtag("config", "G-QL8XR06NXN");
-
-// Initialize Vanta.js effect
-VANTA.RINGS({
-  el: "#background",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200, // Adjusted for full-page coverage
-  minWidth: 200, // Adjusted for full-page coverage
-  scale: 1.0,
-  color: 0x2d2d2d,
-  backgroundColor: 0x06172e,
+   // Initialize Vanta.js effect
+   VANTA.RINGS({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200, // Adjusted for full-page coverage
+    minWidth: 200, // Adjusted for full-page coverage
+    scale: 1.0,
+    color: 0x2d2d2d,
+    backgroundColor: 0x06172e
 });
 
 function copyToClipboard(text) {
-  const el = document.createElement("textarea");
-  el.value = text;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-  alert("متن کپی شد: " + text);
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert('متن کپی شد: ' + text);
 }
 
-const tabButtons = document.querySelectorAll(".tab-button");
-const tabContents = document.querySelectorAll(".tab-pane");
+const tabButtons = document.querySelectorAll('.tab-button');
+const tabContents = document.querySelectorAll('.tab-pane');
 
-tabButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const target = button.dataset.target;
-    tabContents.forEach((tabContent) => {
-      if (tabContent.id === target) {
-        tabContent.classList.remove("hidden");
-      } else {
-        tabContent.classList.add("hidden");
-      }
+tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.dataset.target;
+        tabContents.forEach(tabContent => {
+            if (tabContent.id === target) {
+                tabContent.classList.remove('hidden');
+            } else {
+                tabContent.classList.add('hidden');
+            }
+        });
     });
-  });
 });
 
-const faqQuestions = document.querySelectorAll(".faq-question");
+
+const faqQuestions = document.querySelectorAll('.faq-question');
 
 faqQuestions.forEach((question, index) => {
-  question.addEventListener("click", () => {
-    const answer = question.nextElementSibling;
-    answer.classList.toggle("hidden");
-  });
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        answer.classList.toggle('hidden');
+    });
 });
